e65de4f2d3110238a5670588f4ae96e8bcffa023
Kateřina Rejchrtová <114156741+rejchka1@users.noreply.github.com>
2023-09-12 17:27:12 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 def read_classification_from_file(fpath):
-    with open(fpath, "r", encoding="utf-8") as f:
-        dict = {}
-        for line in f:
-            line = line.replace('\n', '')
+    classification = {}
+    with open(fpath, 'r', encoding='utf-8') as f:
+        for row in f:
+            k, v = row.rstrip().split()
+            classification[k] = v
  
-            key = line.split(' ')[0]
-            value = line.split(' ')[1]
-            dict[key] = value
-    return dict
+    return classification
